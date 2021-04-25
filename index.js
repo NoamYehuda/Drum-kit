0e8d160aac2ef5b5dc614a80c558fcb57f1c5285
NoamYehuda <80532413+NoamYehuda@users.noreply.github.com>
2021-04-25 22:50:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+
+//Detecting button press
+const buttonList = document.querySelectorAll(".drum")
+buttonList.forEach(btn => {
+    btn.addEventListener("click",function () {
+        let word = this.innerHTML
+        sounds(word)
+        buttonAnimation(word)
+    })
+})
+
+//Detecting keybord press
+document.addEventListener("keydown", function (e) {
+    sounds(e.key)
+    buttonAnimation(e.key)
+})
+
+const sounds = function (leter) {
+    switch (leter) {
+        case "w":
+            let tom1 = new Audio('sounds/tom-1.mp3')
+            tom1.play()
+            break;
+
+        case "a":
+            let tom2 = new Audio('sounds/tom-2.mp3')
+            tom2.play()
+            break;
+
+        case "s":
+            let tom3 = new Audio('sounds/tom-3.mp3')
+            tom3.play()
+            break;
+
+        case "d":
+            let tom4 = new Audio('sounds/tom-4.mp3')
+            tom4.play()
+            break;
+
+        case "j":
+            let snare = new Audio('sounds/snare.mp3')
+            snare.play()
+            break;
+
+        case "k":
+            let crash = new Audio('sounds/crash.mp3')
+            crash.play()
+            break;
+
+        case "l":
+            let kickBass = new Audio('sounds/kick-bass.mp3')
+            kickBass.play()
+            break;
+                                                    
+
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector("." + currentKey)
+    activeButton.classList.add("pressed")
+    setTimeout(() => {
+        activeButton.classList.remove("pressed")
+    }, 100);
+}
